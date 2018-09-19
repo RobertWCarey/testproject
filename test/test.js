1b1e873222206616bb11884f35d17d89ba25c472
RobertWCarey <rob_carey_1@hotmail.com>
2018-09-19 19:59:45 +1000

    added npm start

--- test/test.js
+++ test/test.js
@@ -2,4 +2,4 @@ var webdriver = require('selenium-webdriver');
 
 var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
-driver.get('http://movensw.com');
+driver.get('http://localhost:5000/');
